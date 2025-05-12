0e4fc317f73bea66ea34a27240ba90c592afcb99
Ognian Georgiev <ogeorgiev.wrk@gmail.com>
2025-05-12 23:14:01 +0300

    project packages + settings

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+using Unity.Services.Multiplayer;
+using Unity.Multiplayer.Widgets;
+
+namespace Unity.Multiplayer.Widgets {
+    public class Test : MonoBehaviour {
+        private void Start() {
+
+        }
+    }
+}
+
